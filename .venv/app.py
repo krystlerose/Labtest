eb811898164b8078441abefdcc61a642a1cf2091
krystlerose <72145325+krystlerose@users.noreply.github.com>
2022-11-17 18:23:18 +0800

    update

--- .venv/app.py
+++ .venv/app.py
@@ -4,8 +4,12 @@ app=Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 
 if __name__ == '__main__':
